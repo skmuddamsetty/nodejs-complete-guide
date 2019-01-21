1dbc6b1e2db86e9d8c62d822c8851ea1f5a9775f
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2019-01-21 11:18:36 -0500

    Init Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const app = express();
+const path = require('path');
+const rootDir = require('./utils/path');
+// app.use((req, res, next) => {
+//   console.log('inside');
+//   next(); //allows the request to continue to the next middleware inline
+// });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, 'public')));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+});
+// const http = require('http');
+// const server = http.createServer(app);
+// server.listen(3000);
+app.listen(3000); // this can be used instead of above three statements
---
+++ routes/admin.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+const rootDir = require('../utils/path');
+router.get('/add-product', (req, res, next) => {
+  console.log('inside add-product');
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+module.exports = router;
